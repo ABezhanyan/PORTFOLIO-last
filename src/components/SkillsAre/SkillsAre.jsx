c29340f0f99b35a6081cb4271c5c8719b5e2923e
ABezhanyan <hipster24930@gmail.com>
2021-07-30 17:55:21 +0300

    SkillsAre not finished

--- src/components/SkillsAre/SkillsAre.jsx
+++ src/components/SkillsAre/SkillsAre.jsx
@@ -1,14 +1,28 @@
 import './SkillsAre.scss'
-import React, { useRef, useState } from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
-import JS from '../../imgResume/pic.svg'
-import REC from '../../imgResume/pic.svg'
+import JS from '../../imgResume/JS.svg'
+import REC from '../../imgResume/REACT.svg'
+import HTML from '../../imgResume/HTML5.svg'
+import VUE from  '../../imgResume/VUE.svg'
+import NodeJS from '../../imgResume/NodeJS.svg'
+import GIT from '../../imgResume/git.svg'
+import CSS from '../../imgResume/CSS.svg'
+import Angular from '../../imgResume/Angular.svg'
 
 // Import Swiper styles
-import "swiper/swiper.min.css";
+import "swiper/swiper.scss";
 import "swiper/components/effect-coverflow/effect-coverflow.min.css";
 import "swiper/components/pagination/pagination.min.css";
+import "swiper/components/navigation/navigation.min.css";
+import "swiper/components/zoom/zoom.min.css";
+import "swiper/components/a11y/a11y.min.css";
+import "swiper/components/controller/controller.min.css";
+import "swiper/components/scrollbar/scrollbar.min.css";
+import "swiper/components/lazy/lazy.min.css";
+import "swiper/components/thumbs/thumbs.min.css";
+
+
 
 
 // import Swiper core and required modules
@@ -19,36 +33,58 @@ SwiperCore.use([EffectCoverflow, Pagination]);
 
 const SkillsAre = () => {
     return (
-        <>
+        <div className="skills-are-container">
+            <div className="skills-are">
+                <h2 className="skills-are-text">Skills are</h2>
+            </div>
             <Swiper
                 effect={"coverflow"}
                 grabCursor={true}
                 centeredSlides={true}
                 slidesPerView={"auto"}
+                spaceBetween={50}
+                initialSlide={4}
+                parallax={true}
                 coverflowEffect={{
-                    rotate: 0,
+                    rotate: 10,
                     stretch: 10,
-                    depth: 50,
-                    modifier: 3,
-                    slideShadows: true
+                    depth: 100,
+                    modifier: 1,
+                    slideShadows: false,
+
                 }}
                 pagination={true}
                 className="mySwiper"
             >
                 <SwiperSlide className = "pop">
-                    <img src={JS} className="img-sl" />
+                    <img src={JS} className="img-sl" alt="pop" />
                 </SwiperSlide>
                 <SwiperSlide className = "pop">
-                    <img src={REC} className="img-sl" />
+                    <img src={REC} className="img-sl" alt="pop"/>
                 </SwiperSlide>
                 <SwiperSlide className = "pop">
-                    <img src="https://swiperjs.com/demos/images/nature-3.jpg" className="img-sl" />
+                    <img src={HTML} className="img-sl" alt="pop"/>
                 </SwiperSlide>
                 <SwiperSlide className = "pop">
-                    <img src="https://swiperjs.com/demos/images/nature-4.jpg" className="img-sl" />
+                    <img src={VUE} className="img-sl" alt="pop"/>
+                </SwiperSlide>
+                <SwiperSlide className = "pop">
+                    <img src={NodeJS} className="img-sl" alt="pop"/>
+                </SwiperSlide>
+                <SwiperSlide className = "pop">
+                    <img src={GIT} className="img-sl" alt="pop"/>
+                </SwiperSlide>
+                <SwiperSlide className = "pop">
+                    <img src={CSS} className="img-sl" alt="pop"/>
+                </SwiperSlide>
+                <SwiperSlide className = "pop">
+                    <img src={Angular} className="img-sl" alt="pop"/>
+                </SwiperSlide>
+                <SwiperSlide className = "pop">
+                    <img src={Angular} className="img-sl" alt="pop"/>
                 </SwiperSlide>
             </Swiper>
-        </>
+        </div>
     );
 }
 
